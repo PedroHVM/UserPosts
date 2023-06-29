d2ea7e041866f92d563392de9ecb4169dc3abf36
PedroHVM <pedromalheiros987@gmail.com>
2023-06-29 12:24:14 -0300

    Adding the page for each card and a button to go back

--- project.js
+++ project.js
@@ -1,5 +1,3 @@
-const card = document.querySelector('.card');
-card.addEventListener('click', function() {
-  const hiddenText = this.querySelector('.hidden-text');
-  hiddenText.style.display = 'block'; // Ao clicar, exiba o texto oculto
-});
+function go_back() {
+  window.history.back();
+}
